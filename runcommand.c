9d044221b68abdee85b63504226b16597f86442b
darshil179 <darshilpatel179@yahoo.com>
2021-04-26 14:59:25 -0400

    master

--- runcommand.c
+++ runcommand.c
@@ -8,67 +8,134 @@
 #include <errno.h>
 #include <fcntl.h>
 
-
 int runcommand(char **cline, int where)
 {
 
-	if (0 == strcmp(cline[0], "cd")) {
+	if (0 == strcmp(cline[0], "cd"))
+	{
 		int res = chdir(cline[1]);
-					// error handling
+		// error handling
 		switch (res)
 		{
-		case ENOTDIR: perror("A component of path Not a directory");
+		case ENOTDIR:
+			perror("A component of path Not a directory");
 			/* code */
 			break;
 
-		case ENOENT: perror("No such file or directory");
+		case ENOENT:
+			perror("No such file or directory");
 			/* code */
 			break;
 
-		case EACCES: perror("Permission denied");
+		case EACCES:
+			perror("Permission denied");
 			break;
-		case EIO:	 perror("An input output error occured");
+		case EIO:
+			perror("An input output error occured");
 			break;
-		case ENAMETOOLONG: perror("Path is to long");
-			break;		
-		
+		case ENAMETOOLONG:
+			perror("Path is to long");
+			break;
+
 		default:
-		return res;
+			return res;
 			break;
 		}
 	}
 
 	//for requirement 7 "Calculator"
-	if (0 == strcmp(cline[0], "calculator")) {
+	if (0 == strcmp(cline[0], "calculator"))
+	{
 		//char *path = "./dbq4.c"
-		execl("./dbq4","",(char *)NULL);
+		execl("./dbq4", "", (char *)NULL);
 	}
 
-	//for requirement 5 "Calculator"
-	if (0 == strcmp(cline[0], "join")) {
-		char* one[4] = { "ls", "-lt", "more",NULL };
-    
-    char *two[2]={"|", NULL};
+	//for requirement 5 "|"
+	if (0 == strcmp(cline[0], "join"))
+	{
+		char *one[4] = {"ls", "-lt", "more", NULL};
 
-    char* three[2] = { "more", NULL };
+		char *two[2] = {"|", NULL};
 
-    int ret;
+		char *three[2] = {"more", NULL};
 
-    ret = join(one, two,three);
+		int ret;
 
-    printf("\n\njoin returned.\n");
-    exit(0);
-	}
+		ret = join(one, two, three);
 
-	//Requirement 3 user types exit program will terminate
-	if (0 == strcmp(cline[0], "exit")) {
+		printf("\n\njoin returned.\n");
 		exit(0);
 	}
 
+	int join(char *cmd1[], char *cmd2[], char *cmd3[])
+	{
 
-	pid_t pid;
-	int status;
-	switch (pid = fork()) {
+		int fd[2];
+		if ((pid = fork()) == -1)
+		{
+			perror("fork can not be done");
+			exit(1);
+		}
+
+		else if (pid == 0)
+		{
+			if (pipe(fd) == -1)
+			{
+				perror("error in pipe");
+				exit(4);
+			}
+			if (cmd3 != NULL)
+			{
+				if ((pid = fork()) == -1)
+				{
+					perror("fork can not be done");
+					exit(1);
+				}
+
+				else if (pid == 0)
+				{
+					close(fd[0]);
+					dup2(fd[1], 1); //for writing in pipe
+
+					execvp(cmd1[0], cmd1);
+					perror("error in execvp");
+					exit(7);
+				}
+				else
+				{
+					if (pid > 0) //Parent process
+					{
+						close(fd[1]);
+						dup2(fd[0], 0); //reading from pipe
+
+						execvp(cmd2[0], cmd2);
+						perror("error in execvp");
+						exit(7);
+					}
+				}
+			}
+
+			{
+				/* code */
+			}
+		}
+		else
+		{
+			if (pid > 0)
+			{ //Parent process
+			}
+		}
+
+		//Requirement 3 user types exit program will terminate
+		if (0 == strcmp(cline[0], "exit"))
+		{
+			exit(0);
+		}
+
+		pid_t pid;
+		int status;
+		switch (pid = fork())
+		{
 		case -1:
 			perror("smallsh");
 			return (-1);
@@ -76,16 +143,16 @@ int runcommand(char **cline, int where)
 			execvp(cline[0], cline);
 			perror(*cline);
 			exit(1);
+		}
+		//code for parents
+		if (where == BACKGROUND)
+		{
+			printf("[process id %d]\n", pid);
+			return (0);
+		}
+
+		if (waitpid(pid, &status, 0) == -1)
+			return (-1);
+		else
+			return (status);
 	}
-	//code for parents
-	if (where == BACKGROUND)
-	{
-		printf("[process id %d]\n", pid);
-		return (0);
-	}
-	
-	if (waitpid(pid, &status, 0) == -1)
-		return (-1);
-	else
-		return (status);
-}
