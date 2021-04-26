c68a8210228b98f6a4a5fc00998aa3d99ed86e54
darshil179 <darshilpatel179@yahoo.com>
2021-04-26 16:31:26 -0400

    master

--- runcommand.c
+++ runcommand.c
@@ -13,44 +13,7 @@
 int runcommand(char **cline, int where)
 {
 
-	if (0 == strcmp(cline[0], "cd"))
-	{
-		int res = chdir(cline[1]);
-		// error handling
-		switch (res)
-		{
-		case ENOTDIR:
-			perror("A component of path Not a directory");
-			/* code */
-			break;
-
-		case ENOENT:
-			perror("No such file or directory");
-			/* code */
-			break;
-
-		case EACCES:
-			perror("Permission denied");
-			break;
-		case EIO:
-			perror("An input output error occured");
-			break;
-		case ENAMETOOLONG:
-			perror("Path is to long");
-			break;
-
-		default:
-			return res;
-			break;
-		}
-	}
-
-	//for requirement 7 "Calculator"
-	if (0 == strcmp(cline[0], "calculator"))
-	{
-		//char *path = "./dbq4.c"
-		execl("./dbq4", "", (char *)NULL);
-	}
+	
 
 	//for requirement 5 "|"
 
@@ -129,70 +92,6 @@ int runcommand(char **cline, int where)
 		exit(0);
 	}
 
-	// int join(char *cmd1[], char *cmd2[], char *cmd3[])
-	// {
-	// 	int pid1, pid2;
-	// 	int fd[2];
-	// 	if ((pid1 = fork()) == -1)
-	// 	{
-	// 		perror("fork can not be done");
-	// 		exit(1);
-	// 	}
-
-	// 	else if (pid1 == 0)
-	// 	{
-	// 		if (pipe(fd) == -1)
-	// 		{
-	// 			perror("error in pipe");
-	// 			exit(4);
-	// 		}
-	// 		if (cmd3 != NULL)
-	// 		{
-	// 			if ((pid2 = fork()) == -1)
-	// 			{
-	// 				perror("fork can not be done");
-	// 				exit(1);
-	// 			}
-
-	// 			else if (pid2 == 0)
-	// 			{
-	// 				close(fd[0]);
-	// 				dup2(fd[1], 1); //for writing in pipe
-
-	// 				execvp(cmd1[0], cmd1);
-	// 				perror("error in execvp");
-	// 				exit(7);
-	// 			}
-	// 			else
-	// 			{
-	// 				if (pid2 > 0) //Parent process
-	// 				{
-	// 					close(fd[1]);
-	// 					dup2(fd[0], 0); //reading from pipe
-
-	// 					execvp(cmd2[0], cmd2);
-	// 					perror("error in execvp");
-	// 					exit(7);
-	// 				}
-	// 			}
-	// 		}
-
-	// 		{
-	// 			/* code */
-	// 		}
-	// 	}
-	// 	else
-	// 	{
-	// 		if (pid1 > 0)
-	// 		{ //Parent process
-	// 		}
-	// 	}
-
-		//Requirement 3 user types exit program will terminate
-		if (0 == strcmp(cline[0], "exit"))
-		{
-			exit(0);
-		}
 
 		pid_t pid;
 		int status;
